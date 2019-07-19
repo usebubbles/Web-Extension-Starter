7efdf4138810d578166d787fa328a0d0f83319a7
Tom Medema <tommedema@gmail.com>
2019-07-19 16:45:32 -0700

    fix indent

--- utils/locateContentScripts.js
+++ utils/locateContentScripts.js
@@ -1,10 +1,8 @@
 const path = require('path');
 const { readdirSync, lstatSync } = require('fs');
-const { red, yellow, reset, bold, blink, dim} = require('./consoleColors');
+const { red, yellow, reset } = require('./consoleColors');
 
-const isDirectory = (source) => {
-    return lstatSync(source).isDirectory();
-}
+const isDirectory = (source) => lstatSync(source).isDirectory();
 
 /**
  * findContentEntryFile - Will return the index file in a path. Supports files ending with js, ts and tsx.
@@ -14,17 +12,17 @@ const isDirectory = (source) => {
  * @returns - The index file name if exists in the given path. 
  */
 const findContentEntryFile = (contentPath) => {
-    const files = readdirSync(contentPath).filter((name) => /index.(js|ts|tsx)/.test(name));
-    if (!files || files.length < 1) {
-        console.log(`${yellow}Could not find any index files in path:${reset}\n${contentPath}\n`);
-        return undefined;
-    }
-    else if (files.length == 1) {
-        return files.pop();
-    }
-    else {
-        throw `${red}Found ${files.length} index files, ${files}, in path:${reset}\n${contentPath}\nOnly one should be set.\n`;
-    }
+  const files = readdirSync(contentPath).filter((name) => /index.(js|ts|tsx)/.test(name));
+  if (!files || files.length < 1) {
+    console.log(`${yellow}Could not find any index files in path:${reset}\n${contentPath}\n`);
+    return undefined;
+  }
+  else if (files.length == 1) {
+    return files.pop();
+  }
+  else {
+    throw `${red}Found ${files.length} index files, ${files}, in path:${reset}\n${contentPath}\nOnly one should be set.\n`;
+  }
 }
 
 /**
@@ -35,24 +33,24 @@ const findContentEntryFile = (contentPath) => {
  * @returns - A list of objects containing path and folder name
  */
 const getDirectories = (rootPath) => {
-    return readdirSync(rootPath)
-        .map(name => {
-            return {
-                path: path.join(rootPath, name),
-                folderName: name
-            }
-        })
-        .filter((dir) => isDirectory(dir.path));
+  return readdirSync(rootPath)
+    .map(name => {
+      return {
+        path: path.join(rootPath, name),
+        folderName: name
+      }
+    })
+    .filter((dir) => isDirectory(dir.path));
 }
 
 const locateContentScripts = (rootPath) => {
-    const entries = {};
-    const directories = getDirectories(rootPath);
-    directories.forEach((dir) => {
-        const entryFile = findContentEntryFile(dir.path);
-        if (entryFile) { entries[dir.folderName] = path.join(dir.path, entryFile) }
-    })
-    return entries;
+  const entries = {};
+  const directories = getDirectories(rootPath);
+  directories.forEach((dir) => {
+    const entryFile = findContentEntryFile(dir.path);
+    if (entryFile) { entries[dir.folderName] = path.join(dir.path, entryFile) }
+  })
+  return entries;
 }
 
 module.exports = locateContentScripts;
